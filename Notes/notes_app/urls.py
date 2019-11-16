3dc042758019e68bc59fb322d96d3b1b0b197273
unknown <OmarOsman23>
2019-11-16 16:31:09 +0200

    forms

--- Notes/notes_app/urls.py
+++ Notes/notes_app/urls.py
@@ -6,7 +6,8 @@ app_name = 'notes_app'
 
 urlpatterns = [
     url(r'^$', views.all_notes, name='all_notes'),
-    url(r'^(?P<slug>[-\w]+)$', views.note_details, name='note_details'),
     url(r'^add$', views.note_add, name='note_add'),
+    url(r'^(?P<slug>[-\w]+)$', views.note_details, name='note_details'),
+    url(r'^(?P<slug>[-\w]+)/edit$', views.note_edit, name='note_edit'),
 
 ]
--- Notes/notes_app/views.py
+++ Notes/notes_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Note
 from .forms import NoteForm
@@ -24,20 +24,39 @@ def note_details(request, slug):
 
 
 def note_add(request):
-    form = NoteForm
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.user = request.user
+            new_form.save()
+            return redirect('/notes')
+    else:
+        form = NoteForm()
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'note_add.html', context)
 
 
+def note_edit(request, slug):
+    note = get_object_or_404(Note, slug=slug)
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
 
+        if form.is_valid():
+            new_form = form.save(commit=False)
+            new_form.user = request.user
+            new_form.save()
+            return redirect('/notes')
+    else:
+        form = NoteForm(instance = note)
 
+    context = {
+        'form' : form
+    }
 
-
-
-
-
-
-
-
-
-
-
-
+    return render(request, 'note_edit.html', context)
