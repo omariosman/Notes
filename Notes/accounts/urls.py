e00f693b15824854b975396ee8486b5585d5ed15
unknown <OmarOsman23>
2019-11-18 22:11:25 +0200

    Log out

--- Notes/accounts/urls.py
+++ Notes/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'accounts'
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^login/$', LoginView.as_view(), {'template_name' : 'login.html'})
+    url(r'^login/$', LoginView.as_view(), {'template_name' : 'login.html'}),
+    url(r'^logout/$', LogoutView.as_view())
 ]
